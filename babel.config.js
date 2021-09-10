28ea2af561d85e65748ae1881876e2806e4a00fd
qinmingjie <1934394876@qq.com>
2021-09-10 10:29:32 +0800

    router

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-  // presets: [
-  //   '@vue/cli-plugin-babel/preset'
-  // ]
   presets: [
-    ['es2015', { modules: false }]
-  ],
-  plugins: [['component', [
-    {
-      libraryName: 'mint-ui',
-      style: true
-    }
-  ]]]
+    '@vue/cli-plugin-babel/preset'
+  ]
 }
--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
+import router from './router/index.js'
 import 'mint-ui/lib/style.css'
 import { Indicator } from 'mint-ui'
 Vue.use(Indicator)
 Vue.use(VueRouter)
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
